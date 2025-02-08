b3efb8e22af6d389ed48ae5112ae98c7ee0b4400
frodr003 <jbond007@doc.gold.ac.uk>
2025-02-08 16:17:18 +0000

    praparacion de links

--- App.js
+++ App.js
@@ -1,39 +1,19 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,View, ImageBackground } from 'react-native';
+import { StyleSheet,View} from 'react-native';
 import{Main} from "./components/Main"
-// import{Logo} from "./components/Logo"
-import { Carga } from './components/Carga';
 
-const backimag = require('./assets/rm378-08f.jpg')
+
 
 export default function App() {
   return (
-    <Carga>
-      <ImageBackground
-      source={backimag} // Imagen de fondo
-      style={styles.backgroundImage}
-      >
-      <View style={styles.container}>
+
+      <View>
         <StatusBar style='auto' /> 
         {/* <Logo/>  */}
         <Main/>
       </View>
-    </ImageBackground>
-    </Carga>
   );
 }
 
-const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(187, 164, 165, 0.5)', // Color de fondo semi-transparente
-  },
-});
+
